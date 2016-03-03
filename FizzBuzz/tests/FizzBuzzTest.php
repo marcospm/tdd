383bf2062d594f1e8f8305d4e7ebc5bbf7aea955
Marcos Pacheco <marcos.pacheco@trivago.com>
2016-03-03 17:16:11 +0100

    removed content files

--- FizzBuzz/tests/FizzBuzzTest.php
+++ FizzBuzz/tests/FizzBuzzTest.php
@@ -6,56 +6,5 @@ require __DIR__.'/../FizzBuzz.php';
 class FizzBuzzTest extends \PHPUnit_Framework_TestCase
 {
 
-    protected $oFizzBuzz;
-
-    protected function setUp()
-    {
-        $this->oFizzBuzz = new FizzBuzz();
-    }
-
-    public function testTypeClass()
-    {
-        $this->assertInstanceOf('FizzBuzz', $this->oFizzBuzz, 'None expected object type');
-    }
-
-    public function testExecute1()
-    {
-        $this->assertEquals(1, $this->oFizzBuzz->get(1));
-    }
-
-    public function testExecute2()
-    {
-        $this->assertEquals(2, $this->oFizzBuzz->get(2));
-    }
-
-    public function testExecute3()
-    {
-        $this->assertEquals('Fizz', $this->oFizzBuzz->get(3));
-    }
-
-    public function testExecute4()
-    {
-        $this->assertEquals(4, $this->oFizzBuzz->get(4));
-    }
-
-    public function testExecute5()
-    {
-        $this->assertEquals('Buzz', $this->oFizzBuzz->get(5));
-    }
-
-    public function testExecetuteUpTo10()
-    {
-        $this->assertEquals('1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz', $this->oFizzBuzz->upTo('10'));
-    }
-
-    public function testExecute55()
-    {
-        $this->assertEquals('Buzz', $this->oFizzBuzz->get(5.5));
-    }
-
-    public function testExecute33()
-    {
-        $this->assertEquals('Fizz', $this->oFizzBuzz->get(3.3));
-    }
 
 }
