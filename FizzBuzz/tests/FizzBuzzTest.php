4ef00b87c3037072a441f924d041a0551d0fe894
marcospm <mpacheco1988@gmail.com>
2016-03-04 16:14:15 +0100

    Update FizzBuzzTest.php

--- FizzBuzz/tests/FizzBuzzTest.php
+++ FizzBuzz/tests/FizzBuzzTest.php
@@ -5,6 +5,26 @@ require __DIR__.'/../FizzBuzz.php';
 
 class FizzBuzzTest extends \PHPUnit_Framework_TestCase
 {
+    protected $oFizzBuzz;
 
+    protected function setUp()
+    {
+        $this->oFizzBuzz = new FizzBuzz();
+    }
+
+    public function testTypeClass()
+    {
+        $this->assertInstanceOf('FizzBuzz', $this->oFizzBuzz, 'None expected object type');
+    }
+
+    public function testExecute1()
+    {
+        $this->assertEquals(1, $this->oFizzBuzz->get(1));
+    }
+
+    public function testExecute2()
+    {
+        $this->assertEquals(2, $this->oFizzBuzz->get(2));
+    }
 
 }
