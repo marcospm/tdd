5efa429e0a15db8cf9b78e335d7f0a4d8cfc38ba
marcospm <mpacheco1988@gmail.com>
2016-03-04 16:38:45 +0100

    Update FizzBuzz.php

--- FizzBuzz/FizzBuzz.php
+++ FizzBuzz/FizzBuzz.php
@@ -4,6 +4,25 @@ class FizzBuzz
 {
     public function get($number)
     {
+        if ($number == 3)
+        {
+            return 'Fizz';
+        }
+        elseif ($number == 5)
+        {
+            return 'Buzz';
+        }
+
         return $number;
+    }
+
+    public function upTo($number)
+    {
+        $list = [];
+        foreach (range(1, $number) as $i) {
+            $list[] = $this->get($i);
+        }
+
+        return implode(',', $list);
     }
 }
